bea7c9a491b13a24eec0d0fc57acc337c4c95e67
samael33 <IgorOlhovskiy@gmail.com>
2018-01-16 17:56:24 +0200

    ???

--- resources/install/scripts/app/custom/vtiger_connector/ringing.lua
+++ resources/install/scripts/app/custom/vtiger_connector/ringing.lua
@@ -1,10 +1,10 @@
 if (session:ready()) then
-
+	local credentials = {}
 	if (argv[3] == nil or argv[4] == nil) then
 		freeswitch.consoleLog("WARNING", "[vtiger_connector][ringing] Can't get URL or key")
 		do return end
 	end
-	local url, key = argv[3], argv[4]
+	credentials['url'], credentials['key'] = argv[3], argv[4]
 	local dialed_user = session:getVariable("dialed_user")
 	if (dialed_user == nil) then
 		freeswitch.consoleLog("WARNING", "[vtiger_connector][ringing] Can't get dialed user")
@@ -12,13 +12,9 @@ if (session:ready()) then
 	end
 	local call_data = {}
 	call_data['uuid'] = session:getVariable('call_uuid') or ""
-	call_data['number'] = dialed_user
-	call_data['timestamp'] = os.time()
+    call_data['number'] = dialed_user
+    call_data['debug'] = true
 
-	local api_string = url .. "/call_ringing.php content-type application/json post '"..json_encode(call_data).."'"
-	
-	freeswitch.consoleLog("NOTICE", "[vtiger_connector][call_ringing] "..api_string)
-	
-	--api:executeString("bgapi culr "..api_string)
+	vtiger_api_call_ringing(credentials, call_data)
 
 end
