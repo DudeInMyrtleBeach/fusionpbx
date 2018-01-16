0dcd8404be3218e867381db11851b7561b39ed2e
samael33 <IgorOlhovskiy@gmail.com>
2018-01-16 18:07:10 +0200

    API var added

--- resources/install/scripts/app/custom/vtiger_connector/resources/functions/api_functions.lua
+++ resources/install/scripts/app/custom/vtiger_connector/resources/functions/api_functions.lua
@@ -3,6 +3,7 @@
 
 function vtiger_api_call_start(credentials, data)
 
+    local api = freeswitch.API()
     local api_data = data
 
     api_data['timestamp'] = os.time()
@@ -18,6 +19,7 @@ end
 function vtiger_api_call_ringing(credentials, data)
 
     local api_data = data
+    local api = freeswitch.API()
 
     api_data['timestamp'] = os.time()
     local api_string = credentials['url'] .. "call_ringing.php content-type application/json post '"..json_encode(api_data).."'"
@@ -31,6 +33,7 @@ end
 function vtiger_api_call_answer(credentials, data)
 
     local api_data = data
+    local api = freeswitch.API()
 
     api_data['timestamp'] = os.time()
     local api_string = credentials['url'] .. "call_answered.php content-type application/json post '"..json_encode(api_data).."'"
